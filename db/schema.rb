9921085dc891faeaefab8d1810874601aaf38ced
Simon Golz <simon.golz16@gmail.com>
2024-07-12 21:44:23 +0200

    Introduce genres

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_712_132_958) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_12_194207) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'books', force: :cascade do |t|
-    t.string 'isbn'
-    t.string 'title'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "books", force: :cascade do |t|
+    t.string "isbn"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "genre_id", null: false
+    t.index ["genre_id"], name: "index_books_on_genre_id"
   end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "books", "genres"
 end
